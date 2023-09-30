08561380952862f98335da9542c4a5cd72296191
mak22k <64504268+mak22k@users.noreply.github.com>
2023-09-30 19:39:21 -0400

    Add files via upload

---
+++ OOPL_midterm_mkul/OOPL_midterm_mkul/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace OOPL_midterm_mkul
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_output = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.bt_encrypt = new System.Windows.Forms.Button();
+            this.bt_decrypt = new System.Windows.Forms.Button();
+            this.bt_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_input
+            // 
+            this.tb_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_input.Location = new System.Drawing.Point(205, 164);
+            this.tb_input.Multiline = true;
+            this.tb_input.Name = "tb_input";
+            this.tb_input.Size = new System.Drawing.Size(123, 44);
+            this.tb_input.TabIndex = 0;
+            this.tb_input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(102, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter the 4-digit number: ";
+            // 
+            // tb_output
+            // 
+            this.tb_output.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.tb_output.Cursor = System.Windows.Forms.Cursors.No;
+            this.tb_output.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_output.Location = new System.Drawing.Point(26, 307);
+            this.tb_output.Multiline = true;
+            this.tb_output.Name = "tb_output";
+            this.tb_output.ReadOnly = true;
+            this.tb_output.Size = new System.Drawing.Size(478, 72);
+            this.tb_output.TabIndex = 3;
+            this.tb_output.TabStop = false;
+            this.tb_output.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(26, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(478, 81);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TabStop = false;
+            this.textBox3.Text = "Encrypt-O-Matic";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bt_encrypt
+            // 
+            this.bt_encrypt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt_encrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_encrypt.Location = new System.Drawing.Point(26, 236);
+            this.bt_encrypt.Name = "bt_encrypt";
+            this.bt_encrypt.Size = new System.Drawing.Size(119, 43);
+            this.bt_encrypt.TabIndex = 5;
+            this.bt_encrypt.Text = "Encrypt";
+            this.bt_encrypt.UseVisualStyleBackColor = true;
+            this.bt_encrypt.Click += new System.EventHandler(this.bt_encrypt_Click);
+            // 
+            // bt_decrypt
+            // 
+            this.bt_decrypt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt_decrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_decrypt.Location = new System.Drawing.Point(385, 236);
+            this.bt_decrypt.Name = "bt_decrypt";
+            this.bt_decrypt.Size = new System.Drawing.Size(119, 43);
+            this.bt_decrypt.TabIndex = 7;
+            this.bt_decrypt.Text = "Decrypt";
+            this.bt_decrypt.UseVisualStyleBackColor = true;
+            this.bt_decrypt.Click += new System.EventHandler(this.bt_decrypt_Click);
+            // 
+            // bt_clear
+            // 
+            this.bt_clear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_clear.Location = new System.Drawing.Point(209, 236);
+            this.bt_clear.Name = "bt_clear";
+            this.bt_clear.Size = new System.Drawing.Size(119, 43);
+            this.bt_clear.TabIndex = 6;
+            this.bt_clear.Text = "Clear";
+            this.bt_clear.UseVisualStyleBackColor = true;
+            this.bt_clear.Click += new System.EventHandler(this.bt_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 410);
+            this.Controls.Add(this.bt_clear);
+            this.Controls.Add(this.bt_decrypt);
+            this.Controls.Add(this.bt_encrypt);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.tb_output);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_input);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Encrypt-O-Matic";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_output;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button bt_encrypt;
+        private System.Windows.Forms.Button bt_decrypt;
+        private System.Windows.Forms.Button bt_clear;
+    }
+}
+
---
+++ OOPL_midterm_mkul/OOPL_midterm_mkul/Form1.cs
@@ -0,0 +1,148 @@
+﻿// MIDTERM
+// OOPL for Programmers - Spring 2020
+//
+// This program creates a user interface to perform encryption and decryption 
+// 
+// by Marisha Kulseng
+// March 31, 2020
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOPL_midterm_mkul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_clear_Click(object sender, EventArgs e)
+        {
+            tb_input.Text = "";
+            tb_output.Text = "";
+        }
+
+        private void bt_encrypt_Click(object sender, EventArgs e)
+        {
+            if(isValidInput(tb_input.Text))
+                tb_output.Text = encryptInput(tb_input.Text);
+        }
+
+        private void bt_decrypt_Click(object sender, EventArgs e)
+        {
+            if(isValidInput(tb_input.Text))
+                tb_output.Text = decryptInput(tb_input.Text);
+        }
+
+
+        static bool isValidInput(string data)
+        {
+            int test;
+            bool isNotInt = false, isNotCorrectSize = false, 
+                isEmpty = false, showError = false;
+            string errorMsg = "";
+
+            if (data.Equals(""))
+            {
+                isEmpty = true;
+                showError = true;
+            }
+            if (!Int32.TryParse(data, out test)) {
+                isNotInt = true;
+                showError = true;
+            }
+            if (data.Length != 4)
+            {
+                isNotCorrectSize = true;
+                showError = true;
+            }
+
+            if (showError)
+            {
+                if (isEmpty)
+                {
+                    errorMsg += (isEmpty ? " NOT be empty" : "");
+                }
+                else {                    
+                    errorMsg += (isNotInt ? " be NUMERIC" : "");
+                    errorMsg += (isNotCorrectSize ? " be FOUR digits" : "");
+                }
+                                               
+                MessageBox.Show("Input must" + errorMsg + "!", "Error");
+
+                //cleanup section
+                showError = false;
+                return false;
+            }
+            return true;
+        }
+
+        static int encryptDigit(int n)
+        {
+            return ((n + 7) % 10);
+        }
+
+        static int decryptDigit(int n)
+        {
+            return ((n + 3) % 10);
+        }
+
+        static string changeOrder(string data) // works as expected
+        {
+            string result = "";
+            result = data[2].ToString() + data[3].ToString() + data[0].ToString() + data[1].ToString();
+            return result;
+        }
+
+        static int parseInt(string data, int position)
+        {
+            return data[position] - '0';
+        }
+
+        static char convertToChar(int num)
+        {
+            return (char)(num + '0');
+        }
+
+        static string encryptInput(string data)
+        {
+            string result = "";
+            int tempInt;
+            string newOrder = changeOrder(data);
+
+            for (int i = 0; i < 4; i++)
+            {
+                tempInt = encryptDigit(parseInt(newOrder, i));
+                result += tempInt;
+            }
+            return result;
+        }
+
+        static string decryptInput(string data)
+        {
+            string result = "";
+            int tempInt;
+            string newOrder = changeOrder(data);
+
+            for (int i = 0; i < 4; i++)
+            {
+                tempInt = decryptDigit(parseInt(newOrder, i));
+                result += tempInt;
+            }
+            return result;
+        }
+
+
+
+
+    }
+}
